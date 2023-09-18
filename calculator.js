9728e8c98b1ab79904da617cb8900a01f66c09f2
Yasin Arafat Alif <70240790+yasinalif33@users.noreply.github.com>
2023-09-18 23:18:44 +0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function calculator(){
+    const a = 10;
+    const b = 5;
+
+    let add = a + b;
+    let sub = a - b;
+    let mul = a * b;
+    let div = a / b;
+
+    console.log(`
+    Addition: ${add},
+    Substraction: ${sub},
+    Multiplication: ${mul},
+    Division: ${div}`);
+    }
+
+    calculator();
